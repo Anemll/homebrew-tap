595eaca0d2390dbe0d1e5b3b50ed41bf027a80b5
anemll <realanemll@gmail.com>
2026-03-07 19:19:29 -0800

    Add anemll-profile formula v0.1.0

---
+++ Formula/anemll-profile.rb
@@ -0,0 +1,18 @@
+class AnemllProfile < Formula
+  desc "ANE (Apple Neural Engine) CostModel profiler for CoreML models"
+  homepage "https://github.com/Anemll/anemll-profile"
+  url "https://github.com/Anemll/anemll-profile/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "e449f5a32f8f010e57fba9e1333952e08afd140798959353b5e93f8f0a435005"
+  license "MIT"
+
+  depends_on :macos
+  depends_on :xcode => :build
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/anemll-profile 2>&1", 1)
+  end
+end
