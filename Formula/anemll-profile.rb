3b820272ccaa35c09be84a2ad05f33660d6391e0
anemll <realanemll@gmail.com>
2026-03-07 19:26:15 -0800

    Fix SHA256 checksum

--- Formula/anemll-profile.rb
+++ Formula/anemll-profile.rb
@@ -2,7 +2,7 @@ class AnemllProfile < Formula
   desc "ANE (Apple Neural Engine) CostModel profiler for CoreML models"
   homepage "https://github.com/Anemll/anemll-profile"
   url "https://github.com/Anemll/anemll-profile/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "752220a5ca14b4a2c9eac86083540c3cee089964cc94fdb49587866e7dc17f46"
   license "MIT"
 
   depends_on :macos
