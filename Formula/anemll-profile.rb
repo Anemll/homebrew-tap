303a2dde8336cc8a211462182c2f1eaa6f8b9246
anemll <realanemll@gmail.com>
2026-03-07 19:25:34 -0800

    Update SHA256 for v0.1.0

--- Formula/anemll-profile.rb
+++ Formula/anemll-profile.rb
@@ -2,7 +2,7 @@ class AnemllProfile < Formula
   desc "ANE (Apple Neural Engine) CostModel profiler for CoreML models"
   homepage "https://github.com/Anemll/anemll-profile"
   url "https://github.com/Anemll/anemll-profile/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e449f5a32f8f010e57fba9e1333952e08afd140798959353b5e93f8f0a435005"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on :macos
